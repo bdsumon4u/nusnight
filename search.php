52c0c273715d4953d5b1dc8df6ed7cb57df99b96
Sumon Ahmed <bdsumon4u@gmail.com>
2023-10-11 20:02:46 +0600

    Search Function

--- search.php
+++ search.php
@@ -1,10 +1,18 @@
 <?php
 $json = file_get_contents('books.json');
 $bookJson = json_decode($json, true);
+
+$result = [];
+$query = $_GET['query'];
+foreach ($bookJson as $book) {
+    if (stripos($book['title'], $query) !== false) {
+        $result[] = $book;
+    }
+}
 ?>
 <a href="form.php">Add Book</a>
 <form action="search.php" method="get">
-    <input type="text" name="query" id="query">
+    <input type="text" name="query" value="<?php echo $_GET['query'] ?>" id="query">
     <button type="submit">Search</button>
 </form>
 <table border="1" width="900">
@@ -17,7 +25,7 @@ $bookJson = json_decode($json, true);
         <th>Actions</th>
     </tr>
     <?php $index = 0; ?>
-    <?php foreach ($bookJson as $p) {
+    <?php foreach ($result as $p) {
     ?>
         <tr>
             <td><?php echo $p['title']; ?></td>
