44509a2bb0921f0cf478508cc4929fc459ff21c6
Sumon Ahmed <bdsumon4u@gmail.com>
2023-10-11 11:13:13 +0600

    Store new book info

--- createProcess.php
+++ createProcess.php
@@ -4,13 +4,27 @@ $bookJson =json_decode($json,true);
 $title = $_POST['title'];
 $author =$_POST['authorName'];
 $available=$_POST['available'];
-$available=$_POST['pages'];
-$available=$_POST['isbn'];
+$pages=$_POST['pages'];
+$isbn=$_POST['isbn'];
 ?>
 <?php
   if($bookJson)
   {
-     echo "success";
+    $newBookArray = [
+      'title' => $title,
+      'author' => $author,
+      'available' => $available,
+      'pages' => $pages,
+      'isbn' => $isbn,
+    ];
+
+    $bookJson[] = $newBookArray; # append new book info
+
+    $bookJson = json_encode($bookJson); # convert php array to json
+
+    file_put_contents('books.json', $bookJson); # rewrite the json file with new json
+
+    header('Location: index.php'); # redirect to the home (index) page
   }
   else
   echo "Failed";
